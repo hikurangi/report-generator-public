13a4452854c9c0160c4c0b10f41dc10937facebb
Hikurangi <hikurangi@gmail.com>
2020-02-11 16:38:02 +1300

    update webpack config

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,13 +1,18 @@
-const rules = require('./webpack.rules');
+const rules = require('./webpack.rules')
+const plugins = require('./webpack.plugins')
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+})
 
 module.exports = {
   // Put your normal webpack config below here
   module: {
-    rules,
+    rules
   },
-};
+  plugins: plugins,
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
+  }
+}
--- webpack.rules.js
+++ webpack.rules.js
@@ -2,7 +2,7 @@ module.exports = [
   // Add support for native node modules
   {
     test: /\.node$/,
-    use: 'node-loader',
+    use: 'node-loader'
   },
   {
     test: /\.(m?js|node)$/,
@@ -10,25 +10,18 @@ module.exports = [
     use: {
       loader: '@marshallofsound/webpack-asset-relocator-loader',
       options: {
-        outputAssetBase: 'native_modules',
-      },
-    },
-    
+        outputAssetBase: 'native_modules'
+      }
+    }
+  },
+  {
+    test: /\.tsx?$/,
+    exclude: /(node_modules|\.webpack)/,
+    use: {
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true
+      }
+    }
   }
-  // Put your webpack loader rules in this array.  This is where you would put
-  // your ts-loader configuration for instance:
-  /**
-   * Typescript Example:
-   *
-   * {
-   *   test: /\.tsx?$/,
-   *   exclude: /(node_modules|.webpack)/,
-   *   loaders: [{
-   *     loader: 'ts-loader',
-   *     options: {
-   *       transpileOnly: true
-   *     }
-   *   }]
-   * }
-   */
-];
+]
