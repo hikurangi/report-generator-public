37e1963be8146f083d7f094945b06f0284ac877b
Hikurangi <hikurangi@gmail.com>
2020-02-11 16:38:20 +1300

    add comment to install script

--- install-wsl.sh
+++ install-wsl.sh
@@ -1,3 +1,5 @@
+# I install the Windows version of electron, so electron-orge generates
+# a W10 build of the app rather than a Linux one
 npm install && \
 npm uninstall electron && \
 rm package-lock.json && \
