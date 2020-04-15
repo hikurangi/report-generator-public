fb06a5a1ff7bea2133ae607dff97d25836e0921b
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-04-15 14:51:02 +1200

    Remove WSL install script

--- install-wsl.sh
+++
@@ -1,8 +0,0 @@
-# I install the Windows version of electron, so electron-orge generates
-# a W10 build of the app rather than a Linux one
-npm install && \
-npm uninstall electron && \
-rm package-lock.json && \
-export npm_config_platform=win32 && \
-npm install electron -D && \
-unset npm_config_platform
