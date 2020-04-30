6e8892b08cc3765e8a1dea4673518394a85eca64
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-04-30 17:16:07 +1200

    Add package updater shell script

---
+++ update.sh
@@ -0,0 +1,29 @@
+npm i -D @electron-forge/cli@latest \
+@electron-forge/maker-deb@latest \
+@electron-forge/maker-rpm@latest \
+@electron-forge/maker-squirrel@latest \
+@electron-forge/maker-zip@latest \
+@electron-forge/plugin-webpack@latest \
+@marshallofsound/webpack-asset-relocator-loader@latest \
+@types/react@latest \
+@types/react-dom@latest \
+@types/react-router-dom@latest \
+@typescript-eslint/eslint-plugin@latest \
+@typescript-eslint/parser@latest \
+css-loader@latest \
+csstype@latest \
+electron@latest \
+eslint@latest \
+eslint-plugin-import@latest \
+file-loader@latest \
+fork-ts-checker-webpack-plugin@latest \
+node-loader@latest \
+style-loader@latest \
+ts-loader@latest \
+typescript@latest && \
+npm i -S \
+electron-squirrel-startup@latest \
+react@latest \
+react-dom@latest \
+react-router-dom@latest \
+typeface-raleway@latest
