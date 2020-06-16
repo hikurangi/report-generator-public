2d7893aea50aeb45166d94b713a7111a2fdcec76
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-06-16 16:18:33 +1200

    Refactor to make the compiler work, and the code more legible

--- src/services/compile.ts
+++ src/services/compile.ts
@@ -1,23 +1,26 @@
-// import getPronouns from '../model/pronouns'
+import getPronouns from '../model/pronouns'
 import { CompileInputs, ReportNode } from '../types'
 
 const compile = ({ report, reportee } : CompileInputs) : ReportNode => {
-  const patterns = getPronouns(reportee) 
+  const pronouns = getPronouns(reportee) 
+
   let body = report.body ?? ""
 
   if (body.length > 0) {
 
-    body = body.replace(/\{\{(.*?)\}\}/g, (_, captured, offset) => {
+    const pronounPattern = /\{\{(.*?)\}\}/g
 
-      // makes the patterns.get() call below safe
-      if(!patterns.has(captured)) {
-        throw new Error(`Could not find a matching pattern for: "${captured}"`)
+    body = body.replace(pronounPattern, (_, pronounCandidate, pronounOffset) => {
+
+      // makes the pronouns.get() call below safe
+      if(!pronouns.has(pronounCandidate)) {
+        throw new Error(`Could not find a matching pattern for: "${pronounCandidate}"`)
       }
 
-      const doesMatchStartASentence = body.substring(offset - 2, offset) === '. '
+      const doesMatchStartASentence = body.substring(pronounOffset - 2, pronounOffset) === '. '
       // the use of the cast <string> below is a workaround, given the following quirk of ts' Map.get():
       // https://stackoverflow.com/questions/30019542/es6-map-in-typescript#answer-50826619
-      const pronoun = <string>patterns.get(captured)
+      const pronoun = <string>pronouns.get(pronounCandidate)
 
       return doesMatchStartASentence
         ? `${pronoun.charAt(0).toUpperCase()}${pronoun.slice(1)}`
@@ -29,7 +32,7 @@ const compile = ({ report, reportee } : CompileInputs) : ReportNode => {
     // safe to destructure 'cause the return value is typed
     // lollll
     ...report,
-    body: formatted ?? body
+    body
   }
 }
 
