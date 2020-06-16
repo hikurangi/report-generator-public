9aefdafc00c7914b32505414beb7edf9fc5ca174
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-06-16 16:26:41 +1200

    Refactor - document, emove constants out of function scope, type Map for clarity

--- src/services/compile.ts
+++ src/services/compile.ts
@@ -1,14 +1,15 @@
 import getPronouns from '../model/pronouns'
 import { CompileInputs, ReportNode } from '../types'
 
+const pronounPattern = /\{\{(.*?)\}\}/g
+
 const compile = ({ report, reportee } : CompileInputs) : ReportNode => {
-  const pronouns = getPronouns(reportee) 
+  const pronouns: Map<string, string> = getPronouns(reportee) 
 
   let body = report.body ?? ""
 
+  // could remove this 'if' block - just run every time.
   if (body.length > 0) {
-
-    const pronounPattern = /\{\{(.*?)\}\}/g
 
     body = body.replace(pronounPattern, (_, pronounCandidate, pronounOffset) => {
 
@@ -18,6 +19,7 @@ const compile = ({ report, reportee } : CompileInputs) : ReportNode => {
       }
 
       const doesMatchStartASentence = body.substring(pronounOffset - 2, pronounOffset) === '. '
+      
       // the use of the cast <string> below is a workaround, given the following quirk of ts' Map.get():
       // https://stackoverflow.com/questions/30019542/es6-map-in-typescript#answer-50826619
       const pronoun = <string>pronouns.get(pronounCandidate)
