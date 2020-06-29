9239772ec3a10c0f9fede9710c6457ca522e7a30
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-06-29 17:46:23 +1200

    Autoformat layout component

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react'
-import Nav        from '../nav'
-import Main    from '../Main'
-import CSS        from "csstype"
+import Nav from '../nav'
+import Main from '../Main'
+import CSS from "csstype"
 
 const header: CSS.Properties = {
   margin: "50px auto"
 };
 
 const Layout = (): JSX.Element => (
-<main className="container">
-  <section className="row" style={header}>
-    <Nav />
-  </section>
-  <section className="row">
-    <div className="twelve rows">
-      <Main />
-    </div>
-  </section>
-</main>
+  <main className="container">
+    <section className="row" style={header}>
+      <Nav />
+    </section>
+    <section className="row">
+      <div className="twelve rows">
+        <Main />
+      </div>
+    </section>
+  </main>
 )
 
 export default Layout
