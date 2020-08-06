7df543aee0e8cd0ad991cca7488c44a895d87419
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-08-06 13:57:09 +1200

    [WIP] Add basic report setup, ignoring layout for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import * as React                  from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import Reports from './reports'
+// import { BrowserRouter as Router } from 'react-router-dom'
 
-import Layout                      from './layout'
+// import Layout                      from './layout'
 
 const App = (): JSX.Element => (
-  <Router>
-    <Layout />
-  </Router>
-);
+  // <Router>
+  //   <Layout />
+  // </Router>
+  <Reports />
+)
 
 export default App;
--- src/reports/index.tsx
+++ src/reports/index.tsx
@@ -1,7 +1,22 @@
 import * as React from 'react'
+import { ReportNode } from '../types'
+// TODO: consume properly
+import data from '../model/sample-section.json'
+import traverse from '../services/traverse'
+
+// Temporary - should be in a data-fetching abstraction since it will inevitably take place over the wire
+const processedSampleReport: ReportNode = traverse({
+  report: data[0],
+  subject: { name: 'Homer', gender: 'male' }
+})
 
 const Reports = (): JSX.Element => (
-  <h1>Reports</h1>
-) 
+  <>
+    <h1>Reports</h1>
+    <div>
+      <h2>Report 1</h2>
+    </div>
+  </>
+)
 
 export default Reports
